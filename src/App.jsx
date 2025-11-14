39debf69c65b2e2a7093a9414df52f5445394fc9
ElioSpena <elio.spena.94@gmail.com>
2025-11-14 14:15:36 +0100

    feat: chiamato api e recuperato array in una variabile reattiva

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return (
-    <>
-      <h1>prova</h1>
-    </>
-  );
+  const [actresses, setActresses] = useState([]);
+  console.log(actresses);
+
+  useEffect(() => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => {
+      setActresses(resp.data);
+    });
+  }, []);
+
+  return <></>;
 }
 
 export default App;
