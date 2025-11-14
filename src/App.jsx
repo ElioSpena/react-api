edb062ef8953b0cf21e63f258e23286a77ea1de5
ElioSpena <elio.spena.94@gmail.com>
2025-11-14 15:07:38 +0100

    feat: preparato cards e mostrate in pagina

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,32 @@ function App() {
     });
   }, []);
 
-  return <></>;
+  return (
+    <div className="container mt-5">
+      <div className="row g-4">
+        {actresses.map((curActress) => (
+          <div key={curActress.id} className="col-12 col-md-6 col-lg-4">
+            <div className="card h-100">
+              <img
+                src={curActress.image}
+                className="img-fluid rounded-top h-100"
+                alt={curActress.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{curActress.name}</h5>
+                <p className="card-text">{curActress.biography}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{`Birthday: ${curActress.birth_year}`}</li>
+                <li className="list-group-item">{`Nationality: ${curActress.nationality}`}</li>
+                <li className="list-group-item">{`Awards: ${curActress.awards}`}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
